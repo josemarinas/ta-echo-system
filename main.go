6d323b114c01a571f489d1c7b30f9bf2d56a553f
josemarinas <josemarinasbascoy@gmail.com>
2019-10-15 20:53:39 +0200

    Minor changes

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import(
 	log "github.com/sirupsen/logrus"
 	"fmt"
-	"time"
 	"bytes"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -11,7 +10,6 @@ import(
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	. "github.com/logrusorgru/aurora"
-	"strconv"
 	"github.com/rs/xid"
 )
 const sqsMaxMessages int64 = 5
@@ -37,6 +35,7 @@ func main() {
 	go pollQueue(inMsgChan, &inputQueue)
 	for message := range inMsgChan {
 		user := message.MessageAttributes["User"].StringValue
+		command := message.MessageAttributes["Command"].StringValue
 		session := message.Attributes["MessageGroupId"]
 		timestamp := message.Attributes["SentTimestamp"]
 		appendToS3Object(user, session, timestamp, message.Body)
@@ -44,21 +43,17 @@ func main() {
 		log.Infof(
 		"%s message: Body='%s', User='%s', Session='%s', Timestamp='%v'\n",
 		Green("Received"), Blue(*message.Body), Blue(*user), Blue(*session), Blue(*timestamp))
-		sendMessage(message.Body, user, session, &outputQueue)
-
+		sendMessage(message.Body, user, session, &outputQueue, command)
 		log.Infof(
 		"%s message: Body='%s', User='%s', Session='%s', Timestamp='%s'\n",
 		Yellow("Echoed"), Blue(*message.Body), Blue(*user), Blue(*session), Blue(*timestamp))
-		
+			
 		deleteMessage(message.ReceiptHandle, &inputQueue)
-
+	
 		log.Infof(
 		"%s message: Body='%s', User='%s', Session='%s', Timestamp='%s'\n",
 		Red("Deleted"), Blue(*message.Body), Blue(*user), Blue(*session), Blue(*timestamp))
 	}
-	// outMsgChan := make(chan *sqs.Message, sqsMaxMessages)
-	fmt.Println(inputQueue)
-	fmt.Println(outputQueue)
 }
 func appendToS3Object(user *string, session *string, timestamp *string, message *string) {
 	buff := &aws.WriteAtBuffer{}
@@ -67,7 +62,7 @@ func appendToS3Object(user *string, session *string, timestamp *string, message 
 		Key:    aws.String(fmt.Sprintf("/%s/%s.txt", *user, *session)),
 	})
 	if err != nil {
-		log.Errorf("Error downloading S3 object: %v", err)
+		log.Warnf("Error downloading S3 object, if this is the first message of a session ignore this warning: %v", err)
 	}
 	s3Conversation := fmt.Sprintf("%s\n%s\n%s\n%s\n", string(buff.Bytes()), *timestamp, fmt.Sprintf("User: %s", *message), fmt.Sprintf("Echo: %s", *message))
 	_, _ = uploader.Upload(&s3manager.UploadInput{
@@ -76,7 +71,7 @@ func appendToS3Object(user *string, session *string, timestamp *string, message 
 		Key:    aws.String(fmt.Sprintf("/%s/%s.txt", *user, *session)),
 	})
 }
-func sendMessage(message *string, user *string, token *string, queue *string) {
+func sendMessage(message *string, user *string, token *string, queue *string, command *string) {
 		_, err := sqsService.SendMessage(&sqs.SendMessageInput{
 			QueueUrl:            	queue,
 			MessageBody:					message,
@@ -87,18 +82,14 @@ func sendMessage(message *string, user *string, token *string, queue *string) {
 					DataType:    aws.String("String"),
 					StringValue: user,
 				},
-				"Session": &sqs.MessageAttributeValue{
+				"Command": &sqs.MessageAttributeValue{
 						DataType:    aws.String("String"),
-						StringValue: token,
-				},
-				"Timestamp": &sqs.MessageAttributeValue{
-						DataType:    aws.String("String"),
-						StringValue: aws.String(strconv.Itoa(int(time.Now().Unix()))),
+						StringValue: command,
 				},
 			},
 		})
 		if err != nil {
-      log.Errorf("failed to send sqs message %v", err)
+      log.Errorf("Failed to send sqs message %v", err)
 		}
 }
 func deleteMessage(receiptHandle *string, queue *string) {
@@ -107,7 +98,7 @@ func deleteMessage(receiptHandle *string, queue *string) {
 		ReceiptHandle:				receiptHandle,
 	})
 	if err != nil {
-		log.Errorf("failed to delete sqs message %v", err)
+		log.Errorf("Failed to delete sqs message %v", err)
 	}
 }
 
@@ -118,16 +109,19 @@ func pollQueue(chn chan<- *sqs.Message, queue *string) {
       QueueUrl:            		queue,
       MaxNumberOfMessages: 		aws.Int64(sqsMaxMessages),
 			WaitTimeSeconds:     		aws.Int64(sqsPollWaitSeconds),
-			MessageAttributeNames:	aws.StringSlice([]string{"User"}),//,"Session", "SentTimestamp"}),
+			MessageAttributeNames:	aws.StringSlice([]string{"User", "Command"}),//,"Session", "SentTimestamp"}),
     })
 
     if err != nil {
-      log.Errorf("failed to fetch sqs message %v", err)
+      log.Errorf("Failed to fetch sqs message %v", err)
     }
 
     for _, message := range output.Messages {
-			fmt.Println(message)
-      chn <- message
+			if (*message.MessageAttributes["Command"].StringValue == "echo") {
+				chn <- message
+			} else {
+				log.Warnf("Echo system cant handle this request, waiting until timeout")
+			}
     }
   }
 }
